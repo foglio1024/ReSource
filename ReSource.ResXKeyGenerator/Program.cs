eef29c58da36863c3aa6a7764206a49b06d718d4
Foglio <foglio1024@gmail.com>
2021-09-06 14:07:06 +0200

    add ResXKeyGenerator project

---
+++ ReSource.ResXKeyGenerator/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace ReSource.ResXKeyGenerator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputPath = args[0];
+            var outputPath = inputPath.Replace("Designer.cs", "Keys.cs");
+            var className = Path.GetFileNameWithoutExtension(inputPath).Replace(".Designer", "");
+            var sb = new StringBuilder();
+
+            var lines = File.ReadAllLines(inputPath).ToList();
+
+            var namespaceLine = lines.FirstOrDefault(x => x.Trim().StartsWith("namespace"));
+
+
+
+            var propLines = lines.Where(l => l.Trim().StartsWith("public static string ")).ToList();
+
+
+            sb.AppendLine(namespaceLine);
+            sb.AppendLine($"\tpublic class {className}K");
+            sb.AppendLine("\t{");
+
+            propLines.ForEach(l =>
+            {
+                var propName = l.Trim().Split(" ")[3];
+                sb.AppendLine($"\t\t{l.Replace("{", "").Trim()} => nameof({propName});");
+            });
+            sb.AppendLine("\t}");
+            sb.AppendLine("}");
+
+            File.WriteAllText(outputPath, sb.ToString());
+        }
+    }
+}
